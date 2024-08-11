e3d53d5eb51757de851858306069de51d5eb04fc
JustAleksy <143338652+JustAleksy@users.noreply.github.com>
2024-08-11 11:50:49 +0300

    Delete cloud_providers/networking directory

--- cloud_providers/networking/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_vm_ip" {
-  value = yandex_compute_instance.public_vm.network_interface.0.nat_ip_address
-}
-
-output "private_vm_ip" {
-  value = yandex_compute_instance.private_vm.network_interface.0.ip_address
-}
